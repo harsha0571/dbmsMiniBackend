d498ecb03727d707f1d8b423ff38e56fcca7dec4
harsha0571 <harsha90571@gmail.com>
2021-12-11 08:47:05 +0530

    basic crud router added

--- routes/crud.js
+++ routes/crud.js
@@ -1,49 +1,52 @@
 const router = require('express').Router();
+require('dotenv').config()
 
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host: 'brfz6uddsjfdawqruisq-mysql.services.clever-cloud.com',
-        port: 3306,
-        user: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: 'brfz6uddsjfdawqruisq'
-    }
-});
+// const knex = require('knex')({
+//     client: 'mysql2',
+//     connection: {
+//         host: 'brfz6uddsjfdawqruisq-mysql.services.clever-cloud.com',
+//         port: 3306,
+//         user: process.env.USERNAME,
+//         password: process.env.MYSQL_ADDON_PASSWORD,
+//         database: 'brfz6uddsjfdawqruisq'
+//     }
+// });
+
+var knex = require('knex')({
+    client: 'mysql2',
+    connection: process.env.DATABASE_URL,
+    searchPath: 'knex,public',
+    pool: { min: 0, max: 7 }
+})
 
 router.route('/').get((req, res) => {
-    // User.find()
-    //     .then(user => res.json(user))
-    //     .catch(err => res.status(400).json("error " + err))
 
-    knex.from('TEST').select('*').then(no => {
-
-        console.log(no.length)
-        for (var i = 0; i < no.length; i++) {
-            console.log(no[i].phno)
-        }
-
-    });
-
+    knex('TEST').where({}).then(rows => res.json(rows))
+        .catch(err => res.status(400).json("error: " + err))
 
 });
 
+router.route('/').post((req, res) => {
 
-// knex.from('TEST').select('phno')
-//     .then(no => {
-//         console.log(no.length)
-//         for (var i = 0; i < no.length; i++) {
-//             console.log(no[i].phno)
-//         }
+    knex('TEST').insert({ name: req.body.name, phno: req.body.phno })
+        .then(resp => res.json("insertion successful"))
+        .catch(err => res.status(400).json("error:" + err))
 
-//     });
+});
 
-// knex('TEST').insert({ name: 'haddj', phno: 4343 }).then(res => {
-//     console.log(res)
-// });
+router.route('/').put((req, res) => {
+    knex('TEST').where({ name: req.body.name }).update({ phno: req.body.phno })
+        .then(resp => res.json("updation sucessful"))
+        .catch(err => res.status(400).json("error:" + err))
+});
 
-// knex('TEST').where({ name: 'haddj' }).del().then(res => {
-//     console.log(res)
-// });
+router.route('/').delete((req, res) => {
+    knex('TEST').where({ name: req.body.name }).del()
+        .then(resp => res.json("deletion sucessful"))
+        .catch(err => res.status(400).json("error:" + err))
+});
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,16 +6,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host: 'brfz6uddsjfdawqruisq-mysql.services.clever-cloud.com',
-        port: 3306,
-        user: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        database: 'brfz6uddsjfdawqruisq'
-    }
-});
+
 
 const crudRouter = require('./routes/crud');
 app.use('/db', crudRouter)
@@ -26,6 +17,4 @@ app.listen(port, () => {
     console.log(`server running on ${port} `);
 })
 
-module.exports = {
-    knex: knex
-}
+
