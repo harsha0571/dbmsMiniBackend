6ad11abebb260025f9a75e070436f2c64a9adf09
harsha0571 <harsha90571@gmail.com>
2022-01-05 00:50:38 +0530

    added middleware for error handling and token extraction

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const cors = require('cors')
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
-
+const middleware = require('./middleware/utils')
 
 
 
 const crudRouter = require('./routes/crud');
-app.use('/db', crudRouter)
 
+app.use(middleware.tokenExtractor)
+app.use('/db', crudRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const port = process.env.PORT || 5001
 app.listen(port, () => {
