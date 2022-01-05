a5292d3e641254bd9fbed40f0774182eba3bad19
harsha0571 <harsha90571@gmail.com>
2022-01-06 01:50:27 +0530

    added profile query nesting in resigster api

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,25 @@ router.route('/tester').get((req, res) => {
     })
 
 })
+router.route('/profile').get((req, res) => {
+    let sql = "SELECT * FROM profile"
+    db.query(sql, (err, result) => {
+        if (err) res.status(401).json({ err: "not reachable profile" })
+        else {
+            res.status(200).json(result)
+        }
+    })
+})
 
+router.route('/total_time').get((req, res) => {
+    let sql = "SELECT * FROM total_time"
+    db.query(sql, (err, result) => {
+        if (err) res.status(401).json({ err: "not reachable total_time" })
+        else {
+            res.status(200).json(result)
+        }
+    })
+})
 
 router.route('/regUser').post((req, res) => {
 
@@ -34,20 +52,41 @@ router.route('/regUser').post((req, res) => {
         }
 
         if (result) {
-            let getUser = `SELECT * FROM users WHERE username="${body.username}"`
-            var user
+            let getUser = `SELECT * FROM users WHERE username="${body.username}";`
+            let user
             db.query(getUser, (er, rs) => {
-                if (err) res.status(401).json({ err: "user does'nt exist " })
+                if (er) {
+                    console.log(er)
+                    // res.status(401).json({ err: "user does'nt exist " }) 
+                }
                 else {
+
                     user = rs[0]
-                    console.log(user)
-                    console.log(user.user_id)
+                    let date = new Date()
+                    let year = date.getFullYear().toString()
+                    let month = date.getMonth().toString()
+                    let day = (date.getDate() + 1).toString()
+                    let d = year + "-" + month + "-" + day
+
+                    let profile = `INSERT INTO profile (user_id , viewer_name , created_on) VALUES ("${user.user_id}","${user.username}","${d}");`
+
+                    db.query(profile, (erp, rsp) => {
+                        if (erp) {
+                            console.log(erp)
+                            // res.status(401).json({ err: "profile entry not made " })
+                        }
+                        else {
+                            console.log(rsp)
+                        }
+                    })
+
+                    let total_time = `INSERT INTO total_time (user_id) VALUES ("${user.user_id}")`
+
                 }
             })
-            let date = Date.now()
-            console.log("date now :", date)
+
             // console.log(user.user_id)
-            // let sql = `INSERT INTO profile (user_id , viewer_name , created_on) VALUES ("${user.user_id}","${user.name}",,)`
+
             res.status(200).json({
                 message: 'succesful insertion'
             })
